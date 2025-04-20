e62344a4097e43d652b4530197af085088eb5290
Nila Keshavarz <nilabaahaghighat@gmail.com>
2025-04-20 22:08:53 +0330

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+package models;
+
+import models.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class App {
+    private static HashMap<String, User> users = new HashMap<>();
+
+    public HashMap<String, User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User user) {
+        users.put(user.getUsername(), user);
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Scanner;
+import controllers.MenuController;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MenuController menuController = new MenuController(scanner);
+        menuController.run();
+    }
+}
