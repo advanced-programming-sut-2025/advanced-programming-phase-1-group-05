d230e0c5ed650858be17deddf600cc7bbf12af04
saradastgheib <dastgheib.sara@gmail.com>
2025-04-22 09:35:12 +0330

    initialized all npcs

---
+++ models/Enums/Season.java
@@ -0,0 +1,17 @@
+package models.Enums;
+
+public enum Season {
+    SPRING,
+    SUMMER,
+    FALL,
+    WINTER;
+
+    public Season next(){
+        return switch (this){
+            case SPRING ->  SUMMER;
+            case SUMMER ->  FALL;
+            case FALL ->  WINTER;
+            default ->  SPRING;
+        };
+    }
+}
--- models/Item.java
+++ models/Item.java
@@ -1,22 +1,6 @@
 package models;
 
-public class Item {
-    //unique item stuff
-    String name;
-    int price;
-    TimeAndDate producingTime;
-    public Item(String name, int price, TimeAndDate producingTime) {
-        this.name = name;
-        this.price = price;
-        this.producingTime = producingTime;
-    }
-    public String getName() {
-        return name;
-    }
-    public int getPrice() {
-        return price;
-    }
-    public TimeAndDate getProducingTime() {
-        return producingTime;
-    }
+public interface Item {
+    String getName();
+    int getPrice();
 }
