c21bc0cbde09c64b877a671aab722b47f6ee64cd
Nila Keshavarz <nilabaahaghighat@gmail.com>
2025-04-22 21:43:01 +0330

    Update UserDatabase.java

--- models/UserDatabase.java
+++ models/UserDatabase.java
@@ -1,4 +1,20 @@
 package models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserDatabase {
+    private static final Map<String, User> users = new HashMap<>();
+
+    public static void addUser(User user) {
+        users.put(user.getUsername().toLowerCase(), user);
+    }
+
+    public static User getUserByUsername(String username) {
+        return users.get(username.toLowerCase());
+    }
+
+    public static boolean usernameExists(String username) {
+        return users.containsKey(username.toLowerCase());
+    }
 }
