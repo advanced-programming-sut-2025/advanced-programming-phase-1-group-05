f17ba6220febc3c1dd43baedc1cb13542b7278d2
Nila84 <nilabaahaghighat@gmail.com>
2025-04-24 00:01:44 +0330

    main menu,etc

--- src/main/java/org/example/models/UserDatabase.java
+++ src/main/java/org/example/models/UserDatabase.java
@@ -17,4 +17,8 @@ public class UserDatabase {
     public static boolean usernameExists(String username) {
         return users.containsKey(username.toLowerCase());
     }
+
+    public static void updateUser(User user) {
+        users.put(user.getUsername(), user);
+    }
 }
