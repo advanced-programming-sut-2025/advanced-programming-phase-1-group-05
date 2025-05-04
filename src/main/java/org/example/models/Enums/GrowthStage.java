fe4a88ffacff1efe3465f6c353fbaa765013c109
Parisa <parisajalali85@gmail.com>
2025-05-04 18:10:01 +0330

    added plant growth logic + plants json

--- src/main/java/org/example/models/Enums/GrowthStage.java
+++ src/main/java/org/example/models/Enums/GrowthStage.java
@@ -1,4 +1,18 @@
 package org.example.models.Enums;
 
 public enum GrowthStage {
+    SEED(1),
+    SPROUT(2),
+    JUVENILE(3),
+    MATURE(4),
+    BLOOMING(5);
+
+    private final int stageNumber;
+    GrowthStage(int stageNumber) {
+        this.stageNumber = stageNumber;
+    }
+    public int getStageNumber() {
+        return stageNumber;
+    }
+
 }
