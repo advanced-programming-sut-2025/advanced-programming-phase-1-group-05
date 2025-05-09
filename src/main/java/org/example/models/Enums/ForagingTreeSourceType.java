52b2f77eb10dae86d729d069c5e775482b1b1df9
Parisa <parisajalali85@gmail.com>
2025-05-09 21:28:16 +0330

    added foraging tree seeds

---
+++ src/main/java/org/example/models/Enums/ForagingTreeSourceType.java
@@ -0,0 +1,24 @@
+package org.example.models.Enums;
+
+import java.util.List;
+
+public enum ForagingTreeSourceType {
+    Acorns("Acorns", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER)),
+    MapleSeeds("Maple Seeds", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER)),
+    PineCones("Pine Cones", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER)),
+    MahoganySeeds("Mahogany Seeds", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER)),
+    MushroomTreeSeeds("Mushroom Tree Seeds", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER));
+
+    private final String name;
+    private final List<Season> seasons;
+    ForagingTreeSourceType(String name, List<Season> seasons) {
+        this.name = name;
+        this.seasons = seasons;
+    }
+    public String getName() {
+        return name;
+    }
+    public List<Season> getSeasons() {
+        return seasons;
+    }
+}
