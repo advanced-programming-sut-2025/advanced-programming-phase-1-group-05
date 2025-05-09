7cf5b0b9b44df926b2d570bc967c472e6c7c5f9b
Parisa <parisajalali85@gmail.com>
2025-05-09 21:15:56 +0330

    added foraging seeds

---
+++ src/main/java/org/example/models/Enums/ForagingSeedType.java
@@ -0,0 +1,63 @@
+package org.example.models.Enums;
+
+import java.util.List;
+
+public enum ForagingSeedType {
+    JazzSeeds("Jazz Seeds", List.of(Season.SPRING)),
+    CarrotSeeds("Carrot Seeds", List.of(Season.SPRING)),
+    CauliflowerSeeds("Cauliflower Seeds", List.of(Season.SPRING)),
+    CoffeeBean("Coffee Bean", List.of(Season.SPRING)),
+    GarlicSeeds("Garlic Seeds", List.of(Season.SPRING)),
+    BeanStarter("Bean Starter", List.of(Season.SPRING)),
+    KaleSeeds("Kale Seeds", List.of(Season.SPRING)),
+    ParsnipSeeds("Parsnip Seeds", List.of(Season.SPRING)),
+    PotatoSeeds("Potato Seeds", List.of(Season.SPRING)),
+    RhubarbSeeds("Rhubarb Seeds", List.of(Season.SPRING)),
+    StrawberrySeeds("Strawberry Seeds", List.of(Season.SPRING)),
+    TulipBulb("Tulip Bulb", List.of(Season.SPRING)),
+    RiceShoot("Rice Shoot", List.of(Season.SPRING)),
+    BlueBerrySeeds("Blueberry Seeds", List.of(Season.SUMMER)),
+    CornSeeds("Corn Seeds", List.of(Season.SUMMER)),
+    HopsStarter("Hops Starter", List.of(Season.SUMMER)),
+    PepperSeeds("Pepper Seeds", List.of(Season.SUMMER)),
+    MelonSeeds("Melon Seeds", List.of(Season.SUMMER)),
+    PoppySeeds("Poppy Seeds", List.of(Season.SUMMER)),
+    RadishSeeds("Radish Seeds", List.of(Season.SUMMER)),
+    RedCabbageSeeds("Red Cabbage Seeds", List.of(Season.SUMMER)),
+    StarFruitSeeds("Starfruit Seeds", List.of(Season.SUMMER)),
+    SpangleSeeds("Spangle Seeds", List.of(Season.SUMMER)),
+    SummerSquashSeeds("Summer Squash Seeds", List.of(Season.SUMMER)),
+    SunflowerSeeds("Sunflower Seeds", List.of(Season.SUMMER)),
+    TomatoSeeds("Tomato Seeds", List.of(Season.SUMMER)),
+    WheatSeeds("Wheat Seeds", List.of(Season.SUMMER)),
+    AmaranthSeeds("Amaranth Seeds", List.of(Season.FALL)),
+    ArtichokeSeeds("Artichoke Seeds", List.of(Season.FALL)),
+    BeetSeeds("Beet Seeds", List.of(Season.FALL)),
+    BokChoySeeds("Bok Choy Seeds", List.of(Season.FALL)),
+    BroccoliSeeds("Broccoli Seeds", List.of(Season.FALL)),
+    CranberrySeeds("Cranberry Seeds", List.of(Season.FALL)),
+    EggplantSeeds("Eggplant Seeds", List.of(Season.FALL)),
+    FairySeeds("Fairy Seeds", List.of(Season.FALL)),
+    GrapeStarter("Grape Starter", List.of(Season.FALL)),
+    PumpkinSeeds("Pumpkin Seeds", List.of(Season.FALL)),
+    YamSeeds("Yam Seeds", List.of(Season.FALL)),
+    RareSeeds("Rare Seed", List.of(Season.FALL)),
+    PowdermelonSeeds("Powdermelon Seeds", List.of(Season.WINTER)),
+    AncientSeeds("Ancient Seeds", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER)),
+    MixedSeeds("Mixed Seeds", List.of(Season.SPRING, Season.SUMMER, Season.FALL, Season.WINTER));
+
+    private final String name;
+    private final List<Season> seasons;
+    ForagingSeedType(String name, List<Season> seasons) {
+        this.name = name;
+        this.seasons = seasons;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public List<Season> getSeasons() {
+        return seasons;
+    }
+
+}
