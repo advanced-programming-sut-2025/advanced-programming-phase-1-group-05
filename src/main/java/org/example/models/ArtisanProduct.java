513d50ecf331689243c613481b3e512e76e7ff3d
saradastgheib <dastgheib.sara@gmail.com>
2025-05-14 19:48:24 +0330

    starting artisans

--- src/main/java/org/example/models/ArtisanProduct.java
+++ src/main/java/org/example/models/ArtisanProduct.java
@@ -1,4 +1,34 @@
 package org.example.models;
 
-public class ArtisanProduct {
+public class ArtisanProduct implements Item {
+    private final String name;
+    private final int energy;
+    private final int processingHours;
+    private final int sellPrice;
+
+    public ArtisanProduct(String name, int energy, int processingTime, int sellPrice) {
+        this.name = name;
+        this.energy = energy;
+        processingHours = processingTime;
+        this.sellPrice = sellPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return sellPrice;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getProcessingTime() {
+        return processingHours;
+    }
+
+
 }
--- src/main/java/org/example/models/Enums/ArtisanType.java
+++ src/main/java/org/example/models/Enums/ArtisanType.java
@@ -1,5 +1,6 @@
 package org.example.models.Enums;
 
+import org.example.models.ArtisanProduct;
 import org.example.models.Game;
 import org.example.models.Item;
 import org.example.models.Product;
@@ -8,23 +9,23 @@ import java.util.List;
 import java.util.Map;
 
 public enum ArtisanType {
-    BeeHouse(CraftType.BeeHouse, 75, 96, Map.of(),List.of(new Product("Honey", 350, -1, null, null, null))),
-    CheesePress(CraftType.CheesePress, 100, 3, Map.of(Game.getDatabase().getItem("Milk"), 1), List.of(new Product("cheese", 230, -1, null, null, null), new Product("goat cheese", 500, -1, null, null, null))),
-//    Keg(),
+    BeeHouse(CraftType.BeeHouse, List.of(Map.of(Map.of(), new ArtisanProduct("Honey", 75, 96, 350)))),
+    CheesePress(CraftType.CheesePress, List.of(Map.of(Map.of(Game.getDatabase().getItem("milk"), 1), new ArtisanProduct("cheese", 100, 3, 230)),
+            Map.of(Map.of(Game.getDatabase().getItem("large milk"), 1), new ArtisanProduct("cheese", 100, 3, 345)),
+            Map.of(Map.of(Game.getDatabase().getItem("goat milk"),1), new ArtisanProduct("goat cheese", 100, 3, 400)),
+            Map.of(Map.of(Game.getDatabase().getItem("large goat milk"), 1), new ArtisanProduct("goat cheese", 100, 3, 600)))),
+    Keg(CraftType.Keg, ),
 //    Dehydrator(),
 //    CharcoalKiln()
 ;
 
     private final CraftType craftType;
-    private final Map<Item, Integer> items;
-    private int energy;
-    private int hours;
+    private final List<Map<Map<Item, Integer>, ArtisanProduct>> items;
 
-    ArtisanType(CraftType craftType, int energy, int time, Map<Item, Integer> items, List<Product> output) {
+    ArtisanType(CraftType craftType, List<Map<Map<Item, Integer>, ArtisanProduct>> items) {
         this.craftType = craftType;
-        this.energy = energy;
+
         this.items = items;
-        this.hours = time;
 
     }
 }
