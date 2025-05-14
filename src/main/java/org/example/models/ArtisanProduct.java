e7154bfd854c6ba24c397ff637fa136e61211137
saradastgheib <dastgheib.sara@gmail.com>
2025-05-14 22:30:55 +0330

    added all artisan machines

--- src/main/java/org/example/models/ArtisanProduct.java
+++ src/main/java/org/example/models/ArtisanProduct.java
@@ -11,6 +11,7 @@ public class ArtisanProduct implements Item {
         this.energy = energy;
         processingHours = processingTime;
         this.sellPrice = sellPrice;
+        Game.getDatabase().itemDatabase.add(this);
     }
 
     public String getName() {
