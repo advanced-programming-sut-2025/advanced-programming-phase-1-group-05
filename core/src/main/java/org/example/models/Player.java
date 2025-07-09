f5642de717e8930eac8c721fae0b30044f0e8c54
Parisa <parisajalali85@gmail.com>
2025-07-09 20:18:09 +0330

    item in hand and called it a night

--- core/src/main/java/org/example/models/Player.java
+++ core/src/main/java/org/example/models/Player.java
@@ -75,6 +75,7 @@ public class Player {
         backPack.getInventory().put(new Scythe(), 1);
         backPack.getInventory().put(new Axe(), 1);
         backPack.getInventory().put(new WateringCan(), 1);
+        //setCurrentItem(backPack.getFromInventory("Hoe"));
         //TODO fix new row
 //        backPack.getInventory().put(new Food(CookingRecipeType.FruitSalad), 1);
 //        backPack.getInventory().put(new FruitAndVegetable(CropType.Blueberry),1);
@@ -203,28 +204,43 @@ public class Player {
         if (currentAnimation != null) {
             frameToDraw = currentAnimation.getKeyFrame(stateTime, true);
         } else {
-            switch (lastDirection) {
-                case UP:{
-                    frameToDraw = backStill;
-                    break;
-                }
-                case LEFT :{
-                    frameToDraw = leftStill;
-                    break;
-                }
-                case RIGHT :{
-                    frameToDraw = rightStill;
-                    break;
-                }
-                default :{
-                    frameToDraw = frontStill;
-                    break;
-                }
+            if (lastDirection == Direction.UP) {
+                frameToDraw = backStill;
+            } else if (lastDirection == Direction.LEFT) {
+                frameToDraw = leftStill;
+            } else if (lastDirection == Direction.RIGHT) {
+                frameToDraw = rightStill;
+            } else {
+                frameToDraw = frontStill;
             }
         }
 
         batch.draw(frameToDraw, X, Y, width, height);
+
+        if (currentItem != null && currentItem.getTexture() != null) {
+            TextureRegion itemTexture = currentItem.getTexture();
+            float itemSize = 48f;
+            float itemX = X;
+            float itemY = Y;
+
+            if (lastDirection == Direction.UP) {
+                itemX += width / 2f - itemSize / 2f;
+                itemY += height;
+            } else if (lastDirection == Direction.DOWN) {
+                itemX += width / 2f - itemSize / 2f;
+                itemY -= itemSize;
+            } else if (lastDirection == Direction.LEFT) {
+                itemX -= itemSize;
+                itemY += height / 2f - itemSize / 2f;
+            } else if (lastDirection == Direction.RIGHT) {
+                itemX += width;
+                itemY += height / 2f - itemSize / 2f;
+            }
+
+            batch.draw(itemTexture, itemX, itemY, itemSize, itemSize);
+        }
     }
+
 
     public void dispose() {
         texture.dispose();
