fa6c2d6c82a0fe4ca8fe650916b6fe91c2627f8f
Parisa <parisajalali85@gmail.com>
2025-07-25 14:16:05 +0330

    added journal Bg

--- core/src/main/java/org/example/views/GameScreen.java
+++ core/src/main/java/org/example/views/GameScreen.java
@@ -131,6 +131,13 @@ public class GameScreen implements Screen {
     private boolean isCheatCodeOpen = false;
     private TextField cheatCodeTextField;
 
+    //journal stuff
+    private TextureRegion journalBg = GameAssetManager.journalBg;
+    private boolean isJournalOpen = false;
+    private float JOURNAL_X = 0;
+    private float JOURNAL_Y = 0;
+
+
     public GameScreen(ArrayList<Player> playerList) {
         skin = GameAssetManager.getSkin();
         camera = new OrthographicCamera(VIEW_WIDTH * TILE_SIZE, VIEW_HEIGHT * TILE_SIZE);
@@ -282,6 +289,7 @@ public class GameScreen implements Screen {
         showCraftPage(batch);
         showToolSelection(batch);
         showCookingPage(batch);
+        showJournalPage(batch);
         updateToolSelectionSlots();
         checkGifting();
         checkArtisanInput();
@@ -303,6 +311,7 @@ public class GameScreen implements Screen {
             isSkillSetOpen = false;
             isToolSelectionOpen = false;
             isCookingOpen = false;
+            isJournalOpen = false;
             if (isInvenotryOpen) {
                 TextureRegion inventory = MyGame.getCurrentPlayer().getBackPack()
                     .getLevel().getInventoryTexture();
@@ -323,6 +332,7 @@ public class GameScreen implements Screen {
             isInvenotryOpen = false;
             isToolSelectionOpen = false;
             isCookingOpen = false;
+            isJournalOpen = false;
         } else if (Gdx.input.isKeyJustPressed(Input.Keys.T)) {
             GameAssetManager.playSfx("open page");
             isToolSelectionOpen = !isToolSelectionOpen;
@@ -330,6 +340,7 @@ public class GameScreen implements Screen {
             isSkillSetOpen = false;
             isInvenotryOpen = false;
             isCookingOpen = false;
+            isJournalOpen = false;
             updateToolSelectionSlots();
         } else if(Gdx.input.isKeyJustPressed(Input.Keys.B)) {
             GameAssetManager.playSfx("open page");
@@ -338,6 +349,7 @@ public class GameScreen implements Screen {
             isSkillSetOpen = false;
             isInvenotryOpen = false;
             isCookingOpen = false;
+            isJournalOpen = false;
             updateInventorySlots(CRAFT_X, CRAFT_Y);
         } else if(Gdx.input.isKeyJustPressed(Input.Keys.G)) {
             GameAssetManager.playSfx("open page");
@@ -346,7 +358,16 @@ public class GameScreen implements Screen {
             isCraftOpen = false;
             isSkillSetOpen = false;
             isInvenotryOpen = false;
+            isJournalOpen = false;
            updateInventorySlots(COOKING_X,COOKING_Y);
+        } else if(Gdx.input.isKeyJustPressed(Input.Keys.J)) {
+            GameAssetManager.playSfx("open page");
+            isJournalOpen = !isJournalOpen;
+            isCraftOpen = false;
+            isToolSelectionOpen = false;
+            isCraftOpen = false;
+            isSkillSetOpen = false;
+            isInvenotryOpen = false;
         }
         stage.act(delta);
         stage.draw();
@@ -615,6 +636,29 @@ public class GameScreen implements Screen {
         }
 
         batch.end();
+    }
+
+    public void showJournalPage(SpriteBatch batch) {
+        if(!isJournalOpen) return;
+
+        float scale = 0.5f;
+        float drawWidth = journalBg.getRegionWidth() * scale;
+        float drawHeight = journalBg.getRegionHeight() * scale;
+
+        JOURNAL_X = camera.position.x - drawWidth / 2f;
+        JOURNAL_Y = camera.position.y - drawHeight / 2f;
+
+        batch.begin();
+        batch.draw(journalBg, JOURNAL_X, JOURNAL_Y, drawWidth, drawHeight);
+
+        //TODO show quests
+//        Map<String, NPC> quests = new HashMap<>();
+//        List<NPC> npcs = MyGame.getDatabase().getNPCs();
+//        for(NPC npc : npcs) {
+
+//        }
+        batch.end();
+
     }
 
     public void showCookingPage(SpriteBatch batch) {
